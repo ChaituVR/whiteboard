100c0c3957c7f18cb80e1e9f2b0d26c08c6cccc2
Chaitanya Raju <yourchaitu@gmail.com>
2016-04-15 08:55:14 +0000

    Main Goal Done

--- server.js
+++ server.js
@@ -4,104 +4,118 @@ var io = require('socket.io')(http);
 var fs = require('fs');
 
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/client/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
 });
 app.get('/public/js/:filename', function(req, res) {
-  res.sendFile(__dirname + '/client/js/'+req.params.filename);
-  
+    res.sendFile(__dirname + '/client/js/' + req.params.filename);
+
 });
 app.get('/public/css/:filename', function(req, res) {
-  res.sendFile(__dirname + '/client/css/'+req.params.filename);
-  
+    res.sendFile(__dirname + '/client/css/' + req.params.filename);
+
 });
 app.get('/public/img/:filename', function(req, res) {
-  res.sendFile(__dirname + '/client/img/'+req.params.filename);
-  
+    res.sendFile(__dirname + '/client/img/' + req.params.filename);
+
 });
 app.use(function(req, res, next) {
-  res.status(404).send('Sorry cant find that! 404');
+    res.status(404).send('Sorry cant find that! 404');
 });
 
 
-var SocketsList= [];  //JSON.parse(fs.readFileSync('db.txt', 'utf8'));;
+var SocketsList = []; //JSON.parse(fs.readFileSync('db.txt', 'utf8'));;
 
 
 console.log(SocketsList);
-io.on('connection', function(socket){
-  console.log('a user connected');
-  
-    
+io.on('connection', function(socket) {
+    console.log('a user connected');
+
+
     socket.on('adduser', function(username) {
-   // we store the username in the socket session for this client
-   if (username === '' || username === null) {
-    username = 'Guest' + Math.floor((Math.random() * 1000) + 1);
+        // we store the username in the socket session for this client
+        if (username === '' || username === null) {
+            username = 'Guest' + Math.floor((Math.random() * 1000) + 1);
 
-   }
-   socket.username = username;
-  
-  
-   console.log(SocketsList.length);
-  var newPersonSoc= function(username,SockId,mouseX,mouseY){
-      this.username=username;
-      this.SockId=SockId;
-      if (typeof mouseX === 'undefined') { mouseX = 0; }
-      if (typeof mouseY === 'undefined') { mouseY = 0; }
-      this.mouseX=mouseX;
-      this.mouseY=mouseY;
-      
-  };
-  var newPersonOb=new newPersonSoc(socket.username,socket.id);
-  console.log(newPersonOb);
-  updatedb(newPersonOb,"write");
-  
-  
-  });
-  
-  
-  
-   socket.on('mouseMove', function(eventx,eventy){
-    //console.log(eventx + " "+ eventy);
-    
-    socket.broadcast.emit('mouseMove', eventx,eventy);
-     //io.emit('mouseMove');
-  });
-  
-  
-  socket.on('disconnect', function() {
-      console.log('Got disconnect!');
-
-      var i = Socketsearch(socket.id,SocketsList);
-      console.log(i)
-      updatedb(i,"del");
-   });
-  
-  
-  
-  
-});
+        }
+        socket.username = username;
+         socket.idNumber = "user"+makeid();
+        socket.emit('Myusername', socket.username,socket.idNumber);
 
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:3000');
-});
+        var newPersonSoc = function(username,idNumber, mouseX, mouseY) {
+            this.username = username;
+            this.idNumber = idNumber;
+            if (typeof mouseX === 'undefined') {
+                mouseX = Math.floor((Math.random() * 600) + 1);
+            }
+            if (typeof mouseY === 'undefined') {
+                mouseY = Math.floor((Math.random() * 600) + 1);
+            }
+            this.mouseX = mouseX;
+            this.mouseY = mouseY;
+
+        };
+        var newPersonOb = new newPersonSoc(socket.username, socket.idNumber);
+        updatedb(newPersonOb, "write");
+
+
+        // var usersList = getDetails(SocketsList, "username");
+        // var idNumberList = getDetails(SocketsList, "idNumber");
+        io.emit('getUsers',SocketsList);
+        
+        
+
+
+    });
 
 
 
+    socket.on('mouseMove', function(eventx, eventy) {
+        //console.log(eventx + " "+ eventy);
+        
+        
+        socket.broadcast.emit('mouseMove',socket.idNumber,eventx,eventy);
+        //io.emit('mouseMove');
+    });
 
-function updatedb(Person,writeUpDel) {
-
-    if(writeUpDel==="write"){
-    SocketsList.push(Person);
+    function saveMousePostions(id){
+        
+        
     }
-    else if(writeUpDel=="del"){
+    socket.on('disconnect', function() {
+        console.log('Got disconnect!');
+
+        var i = Socketsearch(socket.username, SocketsList);
+        updatedb(i, "del");
+    });
+
+
+
+
+});
+
+
+http.listen(process.env.PORT || 3000, function() {
+    console.log('listening on *:3000');
+});
+
+
+
+
+function updatedb(Person, writeUpDel) {
+
+    if (writeUpDel === "write") {
+        SocketsList.push(Person);
+    }
+    else if (writeUpDel == "del") {
         SocketsList.splice(Person, 1);
     }
-    else if(writeUpDel=="up"){
+    else if (writeUpDel == "up") {
         // 
         //
     }
- var updateusers = JSON.stringify(SocketsList);
+    var updateusers = JSON.stringify(SocketsList);
 
 
     fs.writeFile('db.txt', updateusers, function(err) {
@@ -111,13 +125,42 @@ function updatedb(Person,writeUpDel) {
 
 }
 
-function Socketsearch(socketID, SocketsList){
-    for (var i=0; i < SocketsList.length; i++) {
-        if (SocketsList[i].name === socketID) {
-            console.log(SocketsList[i])
+function Socketsearch(socketID, SocketsList) {
+    for (var i = 0; i < SocketsList.length; i++) {
+        if (SocketsList[i].SockId === socketID) {
             return i;
 
         }
-      
+
     }
+}
+
+// function personSearch(query, SocketsList) {
+//     for (var i = 0; i < SocketsList.length; i++) {
+//         if (SocketsList[i].idNumber === query) {
+//             return i;
+
+//         }
+
+//     }
+// }
+
+function getDetails(SocketsList, key) {
+    var ListOfUsers = [];
+    for (var i = 0; i < SocketsList.length; ++i)
+
+        ListOfUsers.push(SocketsList[i][key]);
+    return ListOfUsers;
+}
+
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 8; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
 }
