1943139b045e485df93c61b6e571e46793c1e875
Chaitanya <yourchaitu@gmail.com>
2017-04-05 12:38:16 +0530

    seperation and helmet :helmet_with_cross:

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,20 @@
 "use strict";
-var express = require('express');
-var path = require("path");
-
+const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+const clientPath = '/../../client/';
 
 module.exports = function(app) {
+
+    app.use(helmet());
     app.use('/public', express.static('client'));
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname + '/../../client/index.html'));
+        res.sendFile(path.join(__dirname + clientPath + 'index.html'));
     });
     app.get('/draw', function(req, res) {
-        res.sendFile(path.join(__dirname + '/../../client/draw.html'));
+        res.sendFile(path.join(__dirname + clientPath + 'draw.html'));
     });
     app.use(function(req, res, next) {
-        res.status(404).sendFile(path.join(__dirname + '/../../client/404.html'));
+        res.status(404).sendFile(path.join(__dirname + clientPath + '404.html'));
     });
 };
