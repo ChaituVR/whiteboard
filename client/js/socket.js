392c4098eb472b17c390ee8ca2982c8a9128502f
Chaitanya <yourchaitu@gmail.com>
2017-04-05 12:38:53 +0530

    seperation and helmet :helmet_with_cross:

---
+++ client/js/socket.js
@@ -0,0 +1,59 @@
+"use strict"
+const socket = io();
+socket.emit('adduser', '' , prompt("What's your room name?"));
+var oldUsers = [];
+$(document).ready(function() {
+  socket.on('Myusername', function(username, Idnumber) {
+    oldUsers.push(Idnumber);
+    console.log('MyUsername : ' + username + " and ID :" + Idnumber);
+  });
+  socket.on('getUsers', function(SocketsList) {
+    // console.log(SocketsList);
+    var allUserIds = getDetails(SocketsList, "idNumber");
+    // console.log(allUserIds);
+    for (var i in SocketsList) {
+      if (oldUsers.indexOf(SocketsList[i].idNumber) == -1) {
+        oldUsers.push(SocketsList[i].idNumber);
+        console.log(oldUsers);
+        $('body').append('<div id="' + SocketsList[i].idNumber + '" class="client-cursor" style="top:' + SocketsList[i].mouseX + 'px;left:' + SocketsList[i].mouseY + 'px;"><img src="public/img/Cursor.png" width="25px" height="23px"><span class="tooltiptext">' + SocketsList[i].username + '</span></div>');
+      }
+    }
+  });
+
+
+  $(document).mousemove(function(event) {
+
+    socket.emit('mouseMove', event.pageX, event.pageY);
+
+  });
+
+
+  socket.on('mouseMove', function(idNumber,eventx,eventy) {
+
+
+    $('#'+idNumber).animate({
+      'left': eventx + 'px',
+      'top': eventy + 'px'
+    }, 0);
+
+  });
+
+  socket.on('drawing', function(idNumber,lastX ,lastY,eventx,eventy) {
+    DrawLine (ctx ,lastX ,lastY , eventx,eventy );
+// console.log(eventx,eventy);
+    // $('#'+idNumber).animate({
+    //   'left': eventx + 'px',
+    //   'top': eventy + 'px'
+    // }, 0);
+
+  });
+
+  function getDetails(SocketsList, key) {
+    var ListOfUsers = [];
+    for (var i = 0; i < SocketsList.length; ++i)
+
+      ListOfUsers.push(SocketsList[i][key]);
+    return ListOfUsers;
+  }
+
+});
