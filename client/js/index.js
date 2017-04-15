40ef2be1d842a33dcf4cd7fc743c0aa45030756c
Chaitanya Raju <yourchaitu@gmail.com>
2017-04-15 11:56:10 +0000

    room id

--- client/js/index.js
+++ client/js/index.js
@@ -1,10 +1,18 @@
 "use strict"
 const createButton = document.querySelector('#createButton');
 const joinButton = document.querySelector('#joinButton');
-createButton.addEventListener('click',function(){
-  document.location.href='/draw';
-});
+const joinRoombtn = document.querySelector('#join-room');
+// createButton.addEventListener('click',function(){
+//   document.location.href='/draw';
+// });
 
-joinButton.addEventListener('click',function(){
-  console.log("ENTER KEY TO JOIN");
-});
+// joinButton.addEventListener('click',function(){
+//   console.log("ENTER KEY TO JOIN");
+  
+// });
+
+joinRoombtn.addEventListener('click',function(){
+    document.location.href='/draw/'+document.querySelector("#roomidField").value;
+})
+console.log(joinRoombtn);
+
