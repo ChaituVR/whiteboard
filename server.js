d4f999176ab4b7ec8d2f82dc4868643eb1bf81ed
Chaitanya <yourchaitu@gmail.com>
2017-04-27 18:57:57 +0530

    Refactor

--- server.js
+++ server.js
@@ -10,19 +10,19 @@ require('./app/routes')(app);
 var SocketsList = [];
 
 console.log(SocketsList);
-io.on('connection', function(socket) {
+io.on('connection', socket => {
   var socketEvents = require('./app/socket');
     socketEvents(socket,SocketsList,io);
 });
 
 var listener = http.listen(process.env.APP_PORT || 8080, (req, res) => {
-    require('dns').lookup(require('os').hostname(), function(err, address, fam) {
+    require('dns').lookup(require('os').hostname(), (err, address, fam) => {
         if (err) console.log(err);
         console.log('We are live on http://' + address + ':' + listener.address().port);
     });
 });
 
-process.on( 'SIGINT', function() {
+process.on( 'SIGINT', () => {
   console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
   // some other closing procedures go here
   process.exit(1);
